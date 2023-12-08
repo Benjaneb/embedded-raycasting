fa5775a2d244ee14e01d0c4c882ec1981fb401f7
HotFazz <basalts.darter-0a@icloud.com>
2023-12-08 11:23:27 +0100

    Doom at 30

--- src/rendering.c
+++ src/rendering.c
@@ -6,6 +6,13 @@
 #define MAP_WIDTH 16
 #define MAP_HEIGHT 16
 
+#define STEP_SIZE 0.01f
+
+typedef struct {
+    float x;
+    float y;
+} vector;
+
 // # is wall
 // . is empty space
 static const char map[MAP_HEIGHT][MAP_WIDTH] = {
@@ -27,17 +34,44 @@ static const char map[MAP_HEIGHT][MAP_WIDTH] = {
     '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
 };
 
-void render_column(color column_buf[DISPLAY_HEIGHT], int screenX, player p, float sinAngle, float cosAngle) {
-    static const float fov = 0.25f * PI;
+void draw_line(int x1, int y1, int x2, int y2) {    // Bresenham's line algorithm
 
-    // Psuedo code for vector solution:
-    
-    // y = tan(FOV / 2) * DISPLAY_WIDTH/2
-    // direction = (x - DISPLAY_WIDTH/2, y)
-    // marching_position = (player.x, player.y)
-    // for distance = 1; distance < MAX_DISTANCE; distance += STEP_SIZE
-    //     marching_position += distance * direction
-    //     if map[floor(marching_position.y)][floor(marching_position.x)] == '#'
-    //         wall_height = DISPLAY_HEIGHT / distance
-    //         draw line from (x, DISPLAY_HEIGHT/2 - wall_height/2) to (x, DISPLAY_HEIGHT/2 + wall_height/2)
+    int dx = x2 - x1;   // delta x
+    int dy = y2 - y1;   // delta y
+
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);  // number of steps to take
+
+    float xIncrement = dx / (float)steps;
+    float yIncrement = dy / (float)steps;
+
+    float x = x1;
+    float y = y1;
+
+    for (int i = 0; i <= steps; i++) {  // draw line
+        draw_pixel(x, y);   
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
+
+void render_column(color column_buf[DISPLAY_HEIGHT], int screenX, player p, float sinAngle, float cosAngle) {   // render a single column
+
+    static const float fov = 0.25f * PI;            // field of view
+    float y = tan(fov / 2) * DISPLAY_WIDTH/2;       // y value of the direction vector
+
+    vector direction = {screenX - DISPLAY_WIDTH/2, y};  // direction vector
+    vector marching_position = {p.x, p.y};              // position vector
+
+    for (float distance = 1; distance < MAX_DISTANCE; distance += STEP_SIZE) {
+
+        marching_position.x += distance * direction.x;
+        marching_position.y += distance * direction.y;
+
+        if (map[(int)floor(marching_position.y)][(int)floor(marching_position.x)] == '#') { // if we hit a wall
+
+            float wall_height = DISPLAY_HEIGHT / distance;
+            draw_line(screenX, DISPLAY_HEIGHT/2 - wall_height/2, screenX, DISPLAY_HEIGHT/2 + wall_height/2);
+            break;
+        }
+    }
 }
