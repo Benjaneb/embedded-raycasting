4f3f671ce47399c1c9940ec26da092620b6fc8e5
HotFazz <basalts.darter-0a@icloud.com>
2023-12-10 15:44:46 +0100

    radical

--- src/rendering.c
+++ src/rendering.c
@@ -4,6 +4,7 @@
 #define PI 3.14159265f
 
 #define MAX_DISTANCE 20
+#define MAX_STEPS 120
 
 #define STEP_SIZE 0.1f
 
@@ -50,6 +51,19 @@ void draw_vertical_line(uint8_t column_buf[DISPLAY_HEIGHT], int start_y, int end
     }
 }
 
+// Use a grid to store the map
+int map_grid[MAP_WIDTH][MAP_HEIGHT];
+
+// Initialize grid
+void init_map_grid() {
+    for (int x = 0; x < MAP_WIDTH; x++) {
+        for (int y = 0; y < MAP_HEIGHT; y++) {
+            map_grid[x][y] = map[x][y] == '#';
+        }
+    }
+}
+
+// Check if a position is a wall using the grid
 int is_wall(float x, float y) {
     int flooredX = (int)floorf(x);
     int flooredY = (int)floorf(y);
@@ -59,7 +73,7 @@ int is_wall(float x, float y) {
         flooredY < 0 || flooredY > MAP_HEIGHT)
         return 0;
     
-    return map[flooredX][flooredY] == '#';
+    return map_grid[flooredX][flooredY];
 }
 
 // Render a single column with raycasting
@@ -81,18 +95,16 @@ void render_column(uint8_t column_buf[DISPLAY_HEIGHT], int screenX, player p, fl
     float y = p.y;
 
     // March in the direction until hitting a wall or reaching MAX_DISTANCE
-    for (float distance = STEP_SIZE; distance < MAX_DISTANCE; distance += STEP_SIZE) {
+    for (int step = 0; step < MAX_STEPS; step++) {
         
         // Move in x and y directions
-        x += stepX * STEP_SIZE * dx;
-        y += stepY * STEP_SIZE * dy;
+        x += stepX * STEP_SIZE;
+        y += stepY * STEP_SIZE;
 
-        if (is_wall(floor(x), floor(y))) {
-            float wall_height = DISPLAY_HEIGHT / (distance * 4.0f);
-            int start_y = DISPLAY_HEIGHT / 2.0f - wall_height / 2.0f;
-            int end_y = DISPLAY_HEIGHT / 2.0f + wall_height / 2.0f;
-            draw_vertical_line(column_buf, start_y, end_y);
-            break;
+        if (is_wall(x, y)) {
+            // Use Manhattan distance
+            float distance = abs(x - p.x) + abs(y - p.y);
+            // ...
         }
     }
-
+}
